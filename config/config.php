110af72e67b26d228cf3aace64e19a230db1234e
quannm9952-max <quannm9952@ut.edu.vn>
2026-06-04 10:34:37 +0700

    đưa dự án web lên git lần đầu

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+define('APP_NAME', 'TechShop');
+define('APP_ENV', 'development');
+define('BASE_URL', 'http://localhost/webbanhang');
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_NAME', 'webbanhang');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
